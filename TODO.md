8b32bbc14e3ced7eb5583a755214ce2306fba726
Leo Ketola <leo.ketola@dnainternet.net>
2026-07-10 20:46:39 +0300

    Basic first person controller and test enviroment.

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO-list test:
+[Godot 4.0 Stable Quick Start Guide ( 2023 )](https://www.youtube.com/watch?v=z_vI3WoAHGY)
+[How To Code An FPS Controller In Godot 4 ](https://www.youtube.com/watch?v=RV-Nwy8N68o&list=PL5lzrGsuEBIGkrL3_35j3Efgo-bPCUZI5&index=1)
+[Fixing Your FPS Controller Camera - Godot Tutorial ](https://www.youtube.com/watch?v=zfIuaRzNti4)
+
+### Credits:
+[Prototype Textures](https://www.kenney.nl/assets/prototype-textures)
---
+++ scripts/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from project settings
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@export var mouse_sense = 0.2
+
+@onready var head: Node3D = $head
+@onready var camera: Camera3D = $head/Camera3D
+
+func _ready() -> void:
+	#Hides the cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sense))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sense))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
